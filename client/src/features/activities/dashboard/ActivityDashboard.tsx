d089979c3d2785ba85ad1825ea958c640c66c832
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-08 17:32:55 -0300

    Create Activity Details and add some images

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,19 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "./details/ActivityDetails";
 
 type Props = {
     activities: Activity[]
 }
 
-export default function ActivityDashboard({activities} : Props) {
+export default function ActivityDashboard({ activities }: Props) {
     return (
-        <Grid2 container>
-            <Grid2 size={9}>
+        <Grid2 container spacing={3}>
+            <Grid2 size={7}>
                 <ActivityList activities={activities} />
+            </Grid2>
+            <Grid2 size={5}>
+                {activities[0] && <ActivityDetails activity={activities[0]} />}
             </Grid2>
         </Grid2>
     )

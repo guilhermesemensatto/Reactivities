28cdd33f9020031bccd00880d437ac740b4b3844
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-07 22:12:50 -0300

    Implement CQRS in Application Layer

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -10,12 +12,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddMediatR(x =>
+    x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>()
+);
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("https://localhost:3000", "http://localhost:3000"));
+app.UseCors(x =>
+    x.AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("https://localhost:3000", "http://localhost:3000")
+);
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
@@ -27,7 +36,7 @@ try
     await context.Database.MigrateAsync();
     await DbInitializer.SeedData(context);
 }
-catch(Exception ex)
+catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occurred during migration");

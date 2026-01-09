bf29fb7e6b8e821a3c403aa30efb87d8e4ac113d
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-09 16:41:28 -0300

    Create Activity form component

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "./details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
     activities: Activity[]
@@ -20,10 +21,13 @@ export default function ActivityDashboard({ activities, cancelSelectActivity, se
                 />
             </Grid2>
             <Grid2 size={5}>
-                {selectedActivity && <ActivityDetails
-                    activity={selectedActivity}
-                    cancelSelectActivity={cancelSelectActivity}
-                />}
+                {selectedActivity &&
+                    <ActivityDetails
+                        activity={selectedActivity}
+                        cancelSelectActivity={cancelSelectActivity}
+                    />
+                }
+                <ActivityForm />
             </Grid2>
         </Grid2>
     )

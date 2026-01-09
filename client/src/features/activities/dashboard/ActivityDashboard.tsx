d194bd986dc9bd55fc6c6454748b45fdddddf343
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-09 16:03:33 -0300

    Add select individual activity to view

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,16 +4,26 @@ import ActivityDetails from "./details/ActivityDetails";
 
 type Props = {
     activities: Activity[]
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    selectedActivity?: Activity
 }
 
-export default function ActivityDashboard({ activities }: Props) {
+export default function ActivityDashboard({ activities, cancelSelectActivity, selectActivity,
+    selectedActivity }: Props) {
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={7}>
-                <ActivityList activities={activities} />
+                <ActivityList
+                    activities={activities}
+                    selectActivity={selectActivity}
+                />
             </Grid2>
             <Grid2 size={5}>
-                {activities[0] && <ActivityDetails activity={activities[0]} />}
+                {selectedActivity && <ActivityDetails
+                    activity={selectedActivity}
+                    cancelSelectActivity={cancelSelectActivity}
+                />}
             </Grid2>
         </Grid2>
     )

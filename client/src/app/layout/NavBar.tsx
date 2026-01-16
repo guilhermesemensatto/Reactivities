47e0e9114ec372c0401a2baae2ed69ae7a7f299b
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-16 00:32:37 -0300

    Create inicial routes

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -6,11 +6,7 @@ import Button from '@mui/material/Button';
 import { Container, MenuItem } from '@mui/material';
 import { Group } from '@mui/icons-material';
 
-type Props = {
-    openForm: () => void;
-}
-
-export default function NavBar({ openForm }: Props) {
+export default function NavBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" sx={{ backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)' }}>
@@ -37,7 +33,7 @@ export default function NavBar({ openForm }: Props) {
                             size="large"
                             variant="contained"
                             color='warning'
-                            onClick={openForm}
+                            onClick={() =>{}}
                         >
                             Create Activity
                         </Button>
--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -2,11 +2,10 @@ import { Box, Button, Card, CardActions, CardContent, Chip, Typography } from "@
 import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
-    activity: Activity
-    selectActivity: (id: string) => void;
+    activity: Activity   
 }
 
-export default function ActivityCard({ activity, selectActivity }: Props) {
+export default function ActivityCard({ activity }: Props) {
     const { deleteActivity } = useActivities();
 
 
@@ -21,7 +20,7 @@ export default function ActivityCard({ activity, selectActivity }: Props) {
             <CardActions sx={{ display: 'flex', justifyContent: 'space-between', pb: 2 }}>
                 <Chip label={activity.category} variant="outlined" />
                 <Box display='flex' gap={3}>
-                    <Button onClick={() => selectActivity(activity.id)} size="medium" variant="contained">View</Button>
+                    <Button onClick={() => {}} size="medium" variant="contained">View</Button>
                     <Button
                         onClick={() => deleteActivity.mutate(activity.id)}
                         disabled={deleteActivity.isPending}

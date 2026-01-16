8e085deb0bce6c27e2d9538606d828cebf798249
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-16 00:46:01 -0300

    Add Navigation links

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -2,9 +2,10 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import { Container, MenuItem } from '@mui/material';
 import { Group } from '@mui/icons-material';
+import { NavLink } from 'react-router';
+import MenuItemLink from '../shared/components/MenuItemLink';
 
 export default function NavBar() {
     return (
@@ -13,30 +14,22 @@ export default function NavBar() {
                 <Container maxWidth='xl'>
                     <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                         <Box>
-                            <MenuItem sx={{ display: 'flex', gap: 2 }}>
+                            <MenuItem component={NavLink} to='/' sx={{ display: 'flex', gap: 2 }}>
                                 <Group fontSize='large' />
                                 <Typography variant="h4" fontWeight='bold'> Reactivities</Typography>
                             </MenuItem>
                         </Box>
                         <Box sx={{ display: 'flex' }}>
-                            <MenuItem sx={{ fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold' }}>
+                            <MenuItemLink to='/activities'>
                                 Activities
-                            </MenuItem>
-                            <MenuItem sx={{ fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold' }}>
-                                About
-                            </MenuItem>
-                            <MenuItem sx={{ fontSize: '1.2rem', textTransform: 'uppercase', fontWeight: 'bold' }}>
-                                Contact
-                            </MenuItem>
+                            </MenuItemLink>
+                            <MenuItemLink to='/createActivity'>
+                                Create Activity
+                            </MenuItemLink>
                         </Box>
-                        <Button
-                            size="large"
-                            variant="contained"
-                            color='warning'
-                            onClick={() =>{}}
-                        >
-                            Create Activity
-                        </Button>
+                        <MenuItem>
+                            User menu
+                        </MenuItem>
                     </Toolbar>
                 </Container>
 

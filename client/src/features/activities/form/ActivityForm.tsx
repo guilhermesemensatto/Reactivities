48191e09749f19bcbd57553f036124506b427500
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-16 01:30:01 -0300

    Add key to create activity route

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -36,7 +36,7 @@ export default function ActivityForm() {
     return (
         <Paper sx={{ borderRadius: 3, padding: 3 }}>
             <Typography variant="h5" gutterBottom color="primary">
-                Create Activity
+                {activity ? 'Edit Activity' : 'Create Activity'}
             </Typography>
             <Box component='form' onSubmit={handleSubmit} display='flex' flexDirection='column' gap={3}>
                 <TextField name='title' label='Tittle' defaultValue={activity?.title} />

c41c1edcc0b27d854b54c5174637b510a2aae080
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-28 00:53:22 -0300

    Add client-side image cropping and preview to photo upload flow

--- client/src/app/shared/components/PhotoUploadWidget.tsx
+++ client/src/app/shared/components/PhotoUploadWidget.tsx
@@ -1,13 +1,22 @@
 import { CloudUpload } from "@mui/icons-material";
 import { Box, Grid2, Typography } from "@mui/material";
-import { useCallback } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useDropzone } from 'react-dropzone'
+import Cropper, { type ReactCropperElement } from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
 export default function PhotoUploadWidget() {
+    const [files, setFiles] = useState<object & { preview: string; }[]>([]);
+    //const cropperRef = useRef<ReactCropperElement>(null);
+
+
     const onDrop = useCallback((acceptedFiles: File[]) => {
-        console.log(acceptedFiles);
-        // Do something with the files
+        setFiles(acceptedFiles.map(file => Object.assign(file, {
+            preview: URL.createObjectURL(file as Blob)
+        })))
     }, [])
+
+
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     return (
@@ -35,9 +44,29 @@ export default function PhotoUploadWidget() {
             </Grid2>
             <Grid2 size={4}>
                 <Typography variant="overline" color="secondary">Step 2 - Resize image</Typography>
+
+                {files[0]?.preview &&
+                    <Cropper
+                        src={files[0]?.preview}
+                        style={{ height: 300, width: '90%' }}
+                        initialAspectRatio={1}
+                        aspectRatio={1}
+                        preview='.img-preview'
+                        guides={false}
+                        viewMode={1}
+                        background={false}
+                    />}
             </Grid2>
             <Grid2 size={4}>
-                <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
+                {files[0]?.preview && (
+                    <>
+                        <Typography variant="overline" color="secondary">Step 3 - Preview & upload</Typography>
+                        <div
+                            className="img-preview"
+                            style={{ height: 300, width: 300, overflow: 'hidden' }}
+                        />
+                    </>
+                )}
             </Grid2>
         </Grid2>
     )

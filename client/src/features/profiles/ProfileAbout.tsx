0b3a423a5bb25c198e1fba3a2fc7b0f2ba1d16ee
Guilherme Semensatto <guilherme.semensatto@codenapp.com>
2026-01-29 15:19:02 -0300

    Integrate edit profile form into about section

--- client/src/features/profiles/ProfileAbout.tsx
+++ client/src/features/profiles/ProfileAbout.tsx
@@ -1,26 +1,36 @@
 import { useParams } from "react-router"
 import { useProfile } from "../../lib/hooks/useProfile";
 import { Box, Button, Divider, Typography } from "@mui/material";
+import { useState } from "react";
+import ProfileEditForm from "./ProfileEditForm";
 
 export default function ProfileAbout() {
     const { id } = useParams();
     const { profile, isCurrentUser } = useProfile(id);
+    const [editProfileMode, setEditProfileMode] = useState(false);
 
     return (
         <Box>
-            <Box display='flex' justifyContent='space-between'>
-                <Typography variant="h5">About {profile?.displayName}</Typography>
-                {isCurrentUser && (<Button>
-                    Edit profile
-                </Button>
+            <Box display="flex" justifyContent="space-between">
+                <Typography variant="h5">
+                    About {profile?.displayName}
+                </Typography>
+                {isCurrentUser && (
+                    <Button onClick={() => setEditProfileMode(!editProfileMode)}>
+                        {editProfileMode ? 'Cancel' : 'Edit profile'}
+                    </Button>
                 )}
             </Box>
             <Divider sx={{ my: 2 }} />
-            <Box sx={{ overflow: 'auto', maxHeight: 350 }}>
-                <Typography variant="body1" sx={{ whiteSpace: 'pre-wrap' }}>
-                    {profile?.bio || 'No description added yet'}
-                </Typography>
-            </Box>
+            {editProfileMode ? (
+                <ProfileEditForm setEditProfileMode={setEditProfileMode} />
+            ) : (
+                <Box sx={{ overflow: 'auto', maxHeight: 350 }}>
+                    <Typography variant="body1" sx={{ whiteSpace: 'pre-wrap' }}>
+                        {profile?.bio || 'No description added yet'}
+                    </Typography>
+                </Box>
+            )}
         </Box>
-    )
+    );
 }
